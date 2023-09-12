ed39707eb6c61f04d05b4eda8e39f3a5a505c3ef
Aleksandr Demakin <alexander.demakin@gmail.com>
2023-09-12 23:49:38 +0100

    src: fix public decls test.

--- src/tests.zig
+++ src/tests.zig
@@ -9,7 +9,7 @@ fn i64Cmp(a: i64, b: i64) math.Order {
 test "test pub decls" {
     var a = std.testing.allocator;
     const TreeType = Tree(i64, i64, i64Cmp);
-    var t = TreeType.init(a, .{});
+    var t = TreeType.init(a);
     defer t.deinit();
     _ = try t.insert(0, 0);
     var it = t.ascendFromStart();
@@ -18,4 +18,8 @@ test "test pub decls" {
     var max = t.getMax().?;
     it = t.descendFromEnd();
     try std.testing.expectEqual(it.prev().?.k, max.k);
+    try std.testing.expectEqual(@as(i64, 0), t.at(0).k);
+    try std.testing.expectEqual(@as(i64, 0), t.at(0).v.*);
+    try std.testing.expectEqual(@as(i64, 0), t.delete(0).?);
+    try std.testing.expectEqual(@as(usize, 0), t.len());
 }
