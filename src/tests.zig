a92ca88e0c856a1ea21a3a91c17df1fce79697af
Aleksandr Demakin <alexander.demakin@gmail.com>
2023-10-11 22:31:09 +0100

    src: deleteAt now returns a KV pair. src: fix some typos and improve comments.

--- src/tests.zig
+++ src/tests.zig
@@ -27,23 +27,28 @@ test "test pub decls" {
 test "tree example usage" {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     defer _ = gpa.detectLeaks();
+    // first, create an i64-->i64 tree
     const TreeType = lib.TreeWithOptions(i64, i64, i64Cmp, .{ .countChildren = true });
     var t = TreeType.init(gpa.allocator());
     defer t.deinit();
+    // add some elements
     var i: i64 = 10;
     while (i >= 0) {
         _ = try t.insert(i, i);
         i -= 1;
     }
+    // get min and max
     if (t.getMin().?.k != 0) {
         @panic("bad min");
     }
     if (t.getMax().?.k != 10) {
         @panic("bad max");
     }
+    // get an element by it's key
     if (t.get(5).?.* != 5) {
         @panic("invalid get result");
     }
+    // iterate
     var it = t.ascendFromStart();
     i = 0;
     while (it.value()) |e| {
@@ -55,5 +60,14 @@ test "tree example usage" {
         }
         i += 1;
         it.next();
+    }
+    // delete by key
+    if (t.delete(0).? != 0) {
+        @panic("invalid delete result");
+    }
+    // delete by position
+    var kv = t.deleteAt(0);
+    if (kv.Key != 1 or kv.Value != 1) {
+        @panic("invalid deleteAt result");
     }
 }
